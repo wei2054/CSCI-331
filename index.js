106fa47619d82db6397286664120f7426ecfd2ca
wei2054 <156958413+wei2054@users.noreply.github.com>
2024-11-02 00:05:34 -0600

    index and counter

---
+++ index.js
@@ -0,0 +1,10 @@
+import Counter from '../components/Counter';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
